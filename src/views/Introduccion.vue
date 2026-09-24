dad9da3876055ceb7dd9d3f14e77a0736ff97486
mafechoSena <mafechoSena@users.noreply.github.com>
2026-09-24 10:59:29 -0500

    video intro

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -28,7 +28,7 @@
 
     figure(data-aos="fade-down")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/79idcHiQyrU?si=V9FtcQpx2S5cYI--" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 </template>
 
 <script>
